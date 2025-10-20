979c4c373bb20e0d930edcea28d72e7eed69a6c7
maria <mmalgbon@epsg.upv.es>
2025-10-20 20:48:44 +0200

    boton acerca de implementado

--- app/src/main/java/com/example/holdy/MainActivity.java
+++ app/src/main/java/com/example/holdy/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.holdy;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton botonAcercaDe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,18 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        botonAcercaDe = findViewById(R.id.botonAcercaDe);
+        botonAcercaDe.setOnClickListener(v -> mensajeBotonAcercaDe());
+
+    }
+
+    private void mensajeBotonAcercaDe() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Acerca de Holdy")
+                .setMessage("Buzón inteligente que recibe y protege tus paquetes, estés donde estés.")
+                .setPositiveButton("Cerrar", (dialog, which) -> dialog.dismiss())
+                .create()
+                .show();
     }
 }
